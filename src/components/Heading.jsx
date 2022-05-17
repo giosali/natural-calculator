3c777db43e60d67900cee0be33b79c47df1e70d1
giosali <70548443+giosali@users.noreply.github.com>
2022-05-16 23:43:43 -0500

    Add Heading component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../stylesheets/Heading.css';
+
+function Heading() {
+  return (
+    <div className="Heading">
+      <h1 className="Heading__h1">Natural Calculator</h1>
+      <p className="Heading__p">Just start typing.</p>
+    </div>
+  );
+}
+
+export default Heading;
